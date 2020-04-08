4cbe950fee85eb92954c15e4a348140589ff2abc
Morteza Q.N <mortezaghorbani3@gmail.com>
2020-04-08 18:04:54 +0430

    Fragment Transaction add and remove

--- app/src/main/java/com/example/sampleanroidfragment/MainActivity.java
+++ app/src/main/java/com/example/sampleanroidfragment/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sampleanroidfragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.frame_main_fragmentContainer, new FragmentA());
+        fragmentTransaction.addToBackStack("FRAGMENT_A");
+        fragmentTransaction.commit();
+
+
+        Button removeTransactionBtn = findViewById(R.id.btn_main_remove_transaction);
+        removeTransactionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.frame_main_fragmentContainer);
+                if (fragment != null) {
+                    FragmentTransaction removeTransaction = getSupportFragmentManager().beginTransaction();
+                    removeTransaction.remove(fragment);
+                    removeTransaction.commit();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sampleanroidfragment/MyFragment.java
+++ app/src/main/java/com/example/sampleanroidfragment/MyFragment.java
@@ -1,21 +1,61 @@
 package com.example.sampleanroidfragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MyFragment extends Fragment {
+
+    private static final String TAG = "MyFragment";
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "onAttach: ");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate: ");
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View fragmentView = inflater.inflate(R.layout.fragment_my_fragment,container,false);
+        Log.i(TAG, "onCreateView: ");
+        View fragmentView = inflater.inflate(R.layout.fragment_my_fragment, container, false);
         return fragmentView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.i(TAG, "onViewCreated: ");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
     }
 }
