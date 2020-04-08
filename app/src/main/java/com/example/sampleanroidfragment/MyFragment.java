4e0a38837e1074f5f32ce08aa16e77ed95c24c5d
Morteza Q.N <mortezaghorbani3@gmail.com>
2020-04-08 15:02:08 +0430

    create MyFragment

---
+++ app/src/main/java/com/example/sampleanroidfragment/MyFragment.java
@@ -0,0 +1,21 @@
+package com.example.sampleanroidfragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View fragmentView = inflater.inflate(R.layout.fragment_my_fragment,container,false);
+        return fragmentView;
+    }
+}
